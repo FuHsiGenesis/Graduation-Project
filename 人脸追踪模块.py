5057c688fd3b46019fc6a35fbcf70d1e237def7d
FuHsiGenesis <1906958932@qq.com>
2021-04-07 21:22:54 +0800

    0407-second-update

--- 人脸追踪模块.py
+++ 人脸追踪模块.py
@@ -8,15 +8,15 @@ from pid import PID
 pan_servo=Servo(1) # P7  X轴
 tilt_servo=Servo(2) # P8  Y轴
 #pan_servo.speed(20)  # 舵机速度初始化
-#tilt_servo.speed(20)
-#pan_servo.calibration(500,2500,500) # 设定允许的最小脉宽、最大脉宽、中间值脉宽
+#tilt_servo.speed(20)  #因为目标物体移动速度不定，所以舵机不能只用一个固定的速度运行所以不需要设置固定速度；
+#pan_servo.calibration(500,2500,500) # 设定允许的最小脉宽、最大脉宽、中间值脉宽--暂时没搞懂，不过不影响现阶段程序运行；
 #tilt_servo.calibration(500,2500,500)
 pan_servo.angle(0)
 tilt_servo.angle(0)
 #pan_pid = PID(p=0.07, i=0, imax=90) #脱机运行或者禁用图像传输，使用这个PID
 #tilt_pid = PID(p=0.05, i=0, imax=90) #脱机运行或者禁用图像传输，使用这个PID
-pan_pid = PID(p=0.1, i=0, imax=90)#在线调试使用这个PID
-tilt_pid = PID(p=0.1, i=0, imax=90)#在线调试使用这个PID
+pan_pid = PID(p=0.1, i=0.1, imax=90)#在线调试使用这个PID
+tilt_pid = PID(p=0.1, i=0.1, imax=90)#在线调试使用这个PID
 
 
 # Reset sensor
@@ -33,7 +33,6 @@ clock = time.clock() # Tracks FPS.
 # 加载Haar算子
 # 默认情况下，这将使用所有阶段，较低的阶段更快但不太准确。
 face_cascade = image.HaarCascade("frontalface", stages=25)
-#print(face_cascade)
 
 
 # 寻找图片中的最大色块：当一帧图片中出现多个人脸时，筛选出面积最大的人脸进行处理；
@@ -70,10 +69,10 @@ while True:
 
         #print("pan_output",pan_output)
         #print("tilt_output",tilt_output)
-        if abs(pan_servo.angle()-pan_output)>=6:
+        if abs(pan_servo.angle()-pan_output)>=0.1:
             pan_servo.angle(pan_servo.angle()-pan_output) #pan_servo.angle()：为上次输出的角度
         print(pan_servo.angle()+pan_output)
-        if abs(tilt_servo.angle()+tilt_output)>=6:
+        if abs(tilt_servo.angle()+tilt_output)>=0.1:
             tilt_servo.angle(tilt_servo.angle()+tilt_output)
         #print('all_output',tilt_servo.angle()+tilt_output)
 
